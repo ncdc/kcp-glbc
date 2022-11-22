7cb3f9b87df65eeab93c8c5f9a4e21f8e74af3c3
Roi Vazquez <roivazquez@gmail.com>
2022-11-22 21:36:03 +0100

    Run glbc in kcp and pass the url and ip to the plugin through the plugin config

--- argocd-glbc-plugin/argocd-glbc-plugin.mk
+++ argocd-glbc-plugin/argocd-glbc-plugin.mk
@@ -4,11 +4,21 @@ K8S_VERSION ?= 1.23.12
 KFILT = docker run --rm -i ryane/kfilt
 
 argocd-plugin-bin:
-	mkdir -p  $(SELF_DIR)argocd-cmp-plugin/bin
+	mkdir -p  $(SELF_DIR)bin
 
-KIND_ADMIN_KUBECONFIG ?= $(SELF_DIR)/kubeconfig
-argocd-start: kind docker-build argocd-cmp-plugin
-	KUBECONFIG=$(KIND_ADMIN_KUBECONFIG) $(KIND) create cluster --wait 5m --config $(SELF_DIR)kind.yaml --image kindest/node:v${K8S_VERSION}
+GLBC_KUBECONFIG ?= $(CLUSTERS_KUBECONFIG_DIR)/kcp-cluster-1.kubeconfig
+argocd-glbc-start:
+	$(MAKE) local-setup NUM_CLUSTERS=1 LOCAL_SETUP_BACKGROUND=1 || true
+	$(KIND) load docker-image ${IMG} --name kcp-cluster-1
+	KUBECONFIG=$(KUBECONFIG) ./utils/deploy.sh -k config/deploy/local
+
+argocd-glbc-stop:
+	pkill kcp
+	$(KIND) delete cluster --name=kcp-cluster-1 || true
+
+ARGOCD_KUBECONFIG ?= $(SELF_DIR)/kubeconfig
+argocd-start: kind argocd-build-plugin
+	KUBECONFIG=$(ARGOCD_KUBECONFIG) $(KIND) create cluster --wait 5m --config $(SELF_DIR)kind.yaml --image kindest/node:v${K8S_VERSION}
 	$(KIND) load docker-image quay.io/kuadrant/kcp-glbc:latest --name kind
 	@make -s argocd-setup
 
@@ -16,24 +26,27 @@ argocd-stop:
 	$(KIND) delete cluster --name=kind || true
 
 argocd-clean:
-	rm -rf $(SELF_DIR)kubeconfig $(SELF_DIR)argocd-cmp-plugin/bin
+	rm -rf $(SELF_DIR)kubeconfig $(SELF_DIR)bin
 
-CMP_PLUGIN ?= $(SELF_DIR)argocd-cmp-plugin/bin/argocd-glbc-plugin
-argocd-cmp-plugin: $(CMP_PLUGIN)
+CMP_PLUGIN ?= $(SELF_DIR)bin/argocd-glbc-plugin
+argocd-build-plugin: $(CMP_PLUGIN)
 $(CMP_PLUGIN): argocd-plugin-bin $(SELF_DIR)plugin.go
-	cd $(SELF_DIR) && GOOS=linux CGO_ENABLED=0 go build -o argocd-cmp-plugin/bin/argocd-glbc-plugin plugin.go
+	cd $(SELF_DIR) && GOOS=linux CGO_ENABLED=0 go build -o bin/argocd-glbc-plugin plugin.go
 	chmod +x $(CMP_PLUGIN)
 
-ARGOCD_PASSWD = $(shell kubectl --kubeconfig=$(KIND_ADMIN_KUBECONFIG) -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+PLUGIN_CONFIG ?= $(SELF_DIR)config/argocd-install/plugin-config.yaml
+GLBC_HOST = $(shell kubectl --kubeconfig $(GLBC_KUBECONFIG) get ingress -A | awk '/ingress-glbc-transform/{print $$4}')
+GLBC_IP = $(shell kubectl --kubeconfig $(GLBC_KUBECONFIG) get ingress -A | awk '/ingress-glbc-transform/{print $$5}')
+argocd-plugin-config:
+	GLBC_HOST=$(GLBC_HOST) GLBC_IP=$(GLBC_IP) envsubst < $(PLUGIN_CONFIG).template > $(PLUGIN_CONFIG)
+
+ARGOCD_PASSWD = $(shell kubectl --kubeconfig=$(ARGOCD_KUBECONFIG) -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 argocd-password:
 	@echo $(ARGOCD_PASSWD)
 
-argocd-setup: export KUBECONFIG=$(KIND_ADMIN_KUBECONFIG)
-argocd-setup: kustomize
-	$(KUSTOMIZE) build $(SELF_DIR)config/ --enable-helm --helm-command $(HELM) | $(KFILT) -k CustomResourceDefinition | kubectl apply -f -
-	@# If we don't wait for cert-manager crds to be installed glbc installation fails
-	# kubectl wait --for condition=established --timeout=60s crd issuers.cert-manager.io
-	$(KUSTOMIZE) build $(SELF_DIR)config/ --enable-helm --helm-command $(HELM) | kubectl apply -f -
+argocd-setup: export KUBECONFIG=$(ARGOCD_KUBECONFIG)
+argocd-setup: kustomize argocd-plugin-config
+	$(KUSTOMIZE) build $(SELF_DIR)config/ | kubectl apply -f -
 	kubectl -n argocd wait deployment argocd-server --for condition=Available=True --timeout=90s
 	kubectl port-forward svc/argocd-server -n argocd 8080:80 > /dev/null  2>&1 &
 	@echo -ne "\n\n\tConnect to ArgoCD UI in https://localhost:8080\n\n"
@@ -45,10 +58,10 @@ argocd-setup: kustomize
 argocd-port-forward-stop:
 	pkill kubectl
 
-argocd-example-glbc-application: export KUBECONFIG=$(KIND_ADMIN_KUBECONFIG)
+argocd-example-glbc-application: export KUBECONFIG=$(ARGOCD_KUBECONFIG)
 argocd-example-glbc-application:
 	kubectl -n argocd apply -f $(SELF_DIR)config/argocd-install/application-glbc-example.yaml
 
-argocd-cmp-logs: export KUBECONFIG=$(KIND_ADMIN_KUBECONFIG)
+argocd-cmp-logs: export KUBECONFIG=$(ARGOCD_KUBECONFIG)
 argocd-cmp-logs:
 	kubectl -n argocd logs -fl "app.kubernetes.io/name=argocd-repo-server" -c plugin
--- argocd-glbc-plugin/plugin.go
+++ argocd-glbc-plugin/plugin.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"log"
+	"net"
+	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/go-resty/resty/v2"
 	"github.com/urfave/cli/v2"
 	yaml "gopkg.in/yaml.v3"
 )
-
-const TransformEndpoint = "http://kcp-glbc-transform.kcp-glbc.svc.cluster.local/transform"
 
 func main() {
 	app := &cli.App{
@@ -22,80 +24,36 @@ func main() {
 			{
 				Name:  "generate",
 				Usage: "argocd-glbc-plugin generate <path>",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "url",
+						Usage:    "the transform endpoint url",
+						Aliases:  []string{"u"},
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "resolve",
+						Usage:    "force the host to resolve to the specified address (format: 'host:port')",
+						Aliases:  []string{"r"},
+						Value:    "",
+						Required: false,
+					},
+					&cli.StringFlag{
+						Name:     "path",
+						Usage:    "the path where the context is",
+						Aliases:  []string{"p"},
+						Value:    ".",
+						Required: false,
+					},
+				},
 				Action: func(cCtx *cli.Context) error {
-					path := cCtx.Args().First()
-					if len(path) < 1 {
-						return cli.Exit("Must specify a path", 1)
-					}
+					path := cCtx.String("path")
+					url := cCtx.String("url")
+					resolve := cCtx.String("resolve")
+
 					// TODO: Sanity check path is not trying to break outside current dir
-					err := filepath.Walk(path,
-						func(file string, info os.FileInfo, err error) error {
-							if err != nil {
-								return err
-							}
-							fileExtension := filepath.Ext(file)
-							if fileExtension == ".yaml" || fileExtension == ".yml" {
 
-								resources, err := os.ReadFile(file)
-								if err != nil {
-									log.Fatal(err)
-								}
-
-								dec := yaml.NewDecoder(bytes.NewReader(resources))
-
-								// a file can contain several yaml documents
-								for {
-									var value map[string]interface{}
-									err := dec.Decode(&value)
-									if err == io.EOF {
-										break
-									}
-									if err != nil {
-										return err
-									}
-
-									// we are only interested in Ingress and Route resources
-									// avoid unmarshalling anything else
-									if kind, ok := value["kind"]; !ok {
-										// should we skip instead??
-										return fmt.Errorf("found document without kind")
-									} else {
-
-										if kind == "Ingress" || kind == "Route" {
-											// Send the resource to the glbc transform endpoint
-											client := resty.New()
-											resp, err := client.R().SetBody(value).Post(TransformEndpoint)
-											if err != nil {
-												return err
-											}
-											// print the transformed resource
-											fmt.Println("---")
-											fmt.Println(string(resp.Body()))
-										} else {
-											// print the resource as-is
-											fmt.Println("---")
-											out, err := yaml.Marshal(value)
-											if err != nil {
-												return fmt.Errorf("error serializing to json: '%s'", err)
-											}
-											fmt.Println(string(out))
-										}
-									}
-								}
-							}
-
-							// TODO: Fetch liveState from ArgoCD ManagedResources API, parsing out any Traffic Objects
-
-							// TODO: Fetch Cluster information from ArgoCD API
-
-							// TODO: Fetch any Applications that are part of the same multi-cluster deployment
-
-							// TODO: Pass Cluster information, multi-cluster Applications, and Traffic Objects targetState and liveState to GLBC transform endpoint
-
-							// TODO: Output transformed version of resources
-
-							return nil
-						})
+					err := generate(path, url, resolve)
 					if err != nil {
 						log.Fatal(err)
 					}
@@ -108,4 +66,104 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func generate(path, url, resolve string) error {
+
+	err := filepath.Walk(path,
+		func(file string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			fileExtension := filepath.Ext(file)
+			if fileExtension == ".yaml" || fileExtension == ".yml" {
+
+				resources, err := os.ReadFile(file)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				dec := yaml.NewDecoder(bytes.NewReader(resources))
+
+				// a file can contain several yaml documents
+				for {
+					var value map[string]interface{}
+					err := dec.Decode(&value)
+					if err == io.EOF {
+						break
+					}
+					if err != nil {
+						return err
+					}
+
+					// we are only interested in Ingress and Route resources
+					// avoid unmarshalling anything else
+					if kind, ok := value["kind"]; !ok {
+						// should we skip instead??
+						return fmt.Errorf("found document without kind")
+					} else {
+
+						if kind == "Ingress" || kind == "Route" {
+							// Send the resource to the glbc transform endpoint
+							client := resty.NewWithClient(client(resolve))
+							resp, err := client.R().SetBody(value).Post(url)
+							if err != nil {
+								return err
+							}
+							if !resp.IsSuccess() {
+								return fmt.Errorf("transform endpoint returned '%s'", resp.Status())
+							}
+							// print the transformed resource
+							fmt.Println("---")
+							fmt.Println(string(resp.Body()))
+						} else {
+							// print the resource as-is
+							fmt.Println("---")
+							out, err := yaml.Marshal(value)
+							if err != nil {
+								return fmt.Errorf("error serializing to json: '%s'", err)
+							}
+							fmt.Println(string(out))
+						}
+					}
+				}
+			}
+
+			// TODO: Fetch liveState from ArgoCD ManagedResources API, parsing out any Traffic Objects
+
+			// TODO: Fetch Cluster information from ArgoCD API
+
+			// TODO: Fetch any Applications that are part of the same multi-cluster deployment
+
+			// TODO: Pass Cluster information, multi-cluster Applications, and Traffic Objects targetState and liveState to GLBC transform endpoint
+
+			// TODO: Output transformed version of resources
+
+			return nil
+		})
+
+	return err
+}
+
+// This is a temporary solution to implement a --resolve option for the plugin
+// like curl's. This is required because the glbc ingress that exposes the transform
+// endpoint doesn't resolve in the local setup. This is only necesary while glbc runs
+// within kcp.
+func client(resolve string) *http.Client {
+
+	dialer := &net.Dialer{Timeout: 2 * time.Second, KeepAlive: 2 * time.Second}
+
+	tr := &http.Transport{
+		Dial: dialer.Dial,
+	}
+
+	tr.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		if resolve != "" {
+			addr = resolve
+		}
+		return dialer.DialContext(ctx, network, addr)
+	}
+
+	client := http.Client{Transport: tr}
+	return &client
 }
